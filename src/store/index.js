28ee8c7be2db7300295dd05d881a61a03eb498dd
Nazareno Busi <nazaabusi@gmail.com>
2023-08-03 19:21:27 -0300

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
+import { authApi } from './auth/api';
+import authReducer from './auth/auth.slice';
 import cartReducer from './cart/cart.slice';
 import { categoriesApi } from './categories/api';
 import categoriesReducer from './categories/categories.slice';
+import { ordersApi } from './orders/api';
+import { productsApi } from './products/api';
 import productsReducer from './products/products.slice';
 
 export const store = configureStore({
@@ -11,9 +15,19 @@ export const store = configureStore({
     products: productsReducer,
     categories: categoriesReducer,
     cart: cartReducer,
+    auth: authReducer,
     [categoriesApi.reducerPath]: categoriesApi.reducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+    [ordersApi.reducerPath]: ordersApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(categoriesApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      categoriesApi.middleware,
+      productsApi.middleware,
+      ordersApi.middleware,
+      authApi.middleware
+    ),
 });
 
 setupListeners(store.dispatch);
